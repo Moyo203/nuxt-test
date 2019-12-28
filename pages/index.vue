6ad218af50771be8dcfec04a20b99d570e4f3c00
Moyo <Moyo203@163.com>
2019-12-28 17:42:06 +0800

    完成首页静态界面

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,49 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        moyo
-      </h1>
-      <h2 class="subtitle">
-        My fabulous Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+	<div class="container">
+        <!-- 幻灯片 -->
+		<!-- interval:间隔时间 -->
+		<!-- arrow: 箭头显示 -->
+        <el-carousel 
+        :interval="5000" 
+        arrow="always">
+            <el-carousel-item 
+            v-for="(item, index) in banners" 
+            :key="index">
+                <div class="banner-image" 
+                :style="`
+                background:url(${item.url}) center center no-repeat;
+                background-size:contain contain;
+                `">
+                </div>
+            </el-carousel-item>
+        </el-carousel>
     </div>
-  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  }
-}
+	data(){
+		return {
+			banners: [
+				{ url: "http://157.122.54.189:9095/assets/images/th01.jfif" },
+				{ url: "http://157.122.54.189:9095/assets/images/th02.jfif" }
+			]
+		}
+	}
+};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+<style scoped lang="less">
+.container{
+    min-width:1000px;
+    margin:0 auto;
+    position:relative;
+    /deep/ .el-carousel__container{
+        height:700px;
+    }
+    .banner-image{
+        width:100%;
+        height:100%;
+    }
 }
 </style>
